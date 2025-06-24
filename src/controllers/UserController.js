38911edce4ea4b5a707627f81218eafd014884e1
Levi Santos <garcialevi65@gmail.com>
2025-06-24 12:31:23 -0300

    Criação da endpoint de cadastro de usuário

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -24,6 +24,29 @@ class UserController {
             return response.status(404).json("Usuário não existe")
         }
     }
+
+    async criar(request, response) {
+        try {
+            let body = request.body
+            const {username, surname, email, password} = body
+
+            if (!username || !surname || !email || !password) {
+                return response.status(400).json({
+                    message: "Todos os campos são obrigatórios!"
+                })
+            }
+
+            await User.create(body)
+            return response.status(201).json({
+                message: "Usuário criado com sucesso!"
+            })
+
+        } catch (error) {
+            return response.status(400).json({
+                message: "Dados da requisição incorretos!"
+            })
+        }
+    }
 }
 
 module.exports = UserController
--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -6,5 +6,6 @@ const userController = new UserController
 
 UserRotas.get('/users', userController.listar)
 UserRotas.get('/users/:id', userController.buscarPorId)
+UserRotas.post('/users', userController.criar)
 
 module.exports = UserRotas
